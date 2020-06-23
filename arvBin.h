cd9011317d630bee413b21c56124704a77b4a343
Maycon Rafael C Borba <mayconrafaelcb@gmail.com>
2020-06-23 19:42:21 -0300

    Estutura trabalho

---
+++ arvBin.h
@@ -0,0 +1,18 @@
+//
+// Created by Mayco on 23/06/2020.
+//
+
+#ifndef ARVORE_ARVBIN_H
+#define ARVORE_ARVBIN_H
+
+#include <stdio.h>
+#include <iostream>
+#include <bits/stdc++.h>
+
+struct arvBin {
+    int chave;
+    arvBin *esq, *dir;
+};
+
+
+#endif //ARVORE_ARVBIN_H
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+#include <bits/stdc++.h>
+#include "arvBin.h"
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
