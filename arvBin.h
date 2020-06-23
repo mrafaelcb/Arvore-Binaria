175f37a1669d75cf28958c07ef1aaa8752798bfe
Maycon Rafael C Borba <mayconrafaelcb@gmail.com>
2020-06-23 20:03:11 -0300

    Feat - Função de inicializar

--- arvBin.h
+++ arvBin.h
@@ -9,10 +9,26 @@
 #include <iostream>
 #include <bits/stdc++.h>
 
+/**
+ * Estrutura da Árvore
+ */
 struct arvBin {
     int chave;
     arvBin *esq, *dir;
 };
 
+/**
+ * Função responsável por iniciar à árvore
+ *
+ * @return arvBin
+ */
+arvBin **iniciaArv() {
+    arvBin **ptrRaiz = new arvBin *;
+    if (*ptrRaiz != NULL) {
+        *ptrRaiz = NULL;
+    }
+
+    return ptrRaiz;
+}
 
 #endif //ARVORE_ARVBIN_H
--- main.cpp
+++ main.cpp
@@ -6,6 +6,33 @@
 using namespace std;
 
 int main() {
+    arvBin **ptrRaiz;
+    int escolha = 1;
+
+    while (escolha != 0) {
+        printf("Escolha a opção:");
+        scanf("%d ", &escolha);
+        switch (escolha) {
+            case 0: {
+                printf("Adeus.\n");
+            }
+                break;
+            case 1: {
+                printf("struct arvBin{\n");
+                printf("\tint chave;\n\tarvBin *esq, *dir;\n}\n");
+            }
+                break;
+            case 2: {
+                if (ptrRaiz != NULL) {
+                    printf("Arvore ja inicializada!\n");
+                } else {
+                    ptrRaiz = iniciaArv();
+                    printf("Arvore inicializada com sucesso!\n");
+                }
+            }
+                break;
+        }
+    }
 
     return 0;
 }
